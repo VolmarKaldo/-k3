4b7bf4e43a932a79c0613d6cd166e9a678c0657f
volmar.kaldo <volmar.kaldo@elev.ga.ntig.se>
2021-10-04 13:21:30 +0200

    Commit 4/10

---
+++ Listor/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.AccessControl;
+using System;
+using System.Collections.Generics;
+
+namespace Listor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] städer = {"Berlin", "Paris", "London"};
+            
+            string[] frukter = new string[3];
+            frukter [0] = "Äpple";
+            frukter [1] = "Apelsin";
+            frukter [2] = "Banan";
+        
+            List<string> namnen = new list<string>(){"Miriam", "Teo", "Axel"};
+
+            List<string> bären = new List<string>();
+            bären.Add("Blåbär");
+            bären.Add("Lingon");
+            bären.Add("Hallon");
+            bären.Add("Björnbär");
+            bären.Add("Kråkbär");
+
+            for (int i = 0; i < frukter.Length; i++)
+            {
+                System.Console.WriteLine(frukter[i]);
+            }
+            foreach (var frukt in frukter)
+            {
+                System.Console.WriteLine(frukt);
+            }
+
+            for (int i = 0; i < bären.count; i++)
+            {
+                System.Console.WriteLine(bären[i]);
+            }
+
+            foreach (var stad in städer)
+            {
+               if (stad=="Paris")
+               {
+                   System.Console.WriteLine("Paris finns i arrayen"); 
+               } 
+            }
+            if (bären.Contains("Blåbär"))
+            {
+                System.Console.WriteLine("Blåbär finns i listan");
+            }
+
+            bären.Remove("Hallon");
+            bären.RemoveAt(0);
+            bären.Insert(1, "Aroniabär");
+        }
+    }
+}
---
+++ SamlaNamn/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SamlaNamn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Global lista
+        List<string> namnLista = new List<string>();
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void ClickLäggTill(object sender, RoutedEventArgs e)
+        {
+            //Läs av rutaNamn
+            string namn = rutaNamn.Text;
+            //Om den inte är tom, lägg till i rutaLista
+
+            //Och lagra i en lista
+            if (namn != "")
+            {
+                rutaLista.Text += $"{namn}\n";
+                namnLista.Add(namn);
+            }
+            else
+            {
+                rutaFel.Text = "Fältet är tomt.";
+            }
+        }
+        private void ClickSpara(Object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines("namnlista.txt", namnLista);
+            rutaFel.Text = "Namnen sparade i namnlista.txt";
+        }
+    }
+}
